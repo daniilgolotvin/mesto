6b714fc8ee3b9fb0bbde565181dad1b8e0d4dc35
DANIIL GOLOTVIN <daniilgolotvin@yandex.ru>
2023-03-13 00:09:53 +0300

    firsTry

--- scripts/index.js
+++ scripts/index.js
@@ -2,44 +2,106 @@ const page = document.querySelector('.page');
 
 const popup = document.querySelector('#popup');
 
-const openPopupButton = document.querySelector('#open-popup');
+const profilePopup = document.querySelector('.profile-popup');
 
-const closePopupButton = document.querySelector('#close-popup');
+const addPopup = document.querySelector('.add-popup');
 
-let profileNameElement = document.querySelector('#profile-name'); //выводим это все на страницу
+const addName = addPopup.querySelector('.add-name');
 
-let profileDescriptionElement = document.querySelector('#profile-description');
+const addLink = addPopup.querySelector('.add-link');
 
-let userName = document.querySelector('#user-name'); // первое поле ввода
+const editButton = document.querySelector('#open-popup');
 
-let userPost = document.querySelector('#user-post'); //второе поля ввода
+const closeProfileButton = document.querySelector('#close-popup');
 
-let popupWindow = document.querySelector('.popup__window');
+const closeAddButton = document.querySelector('#close-addPopup');
 
-function openPopup() {
+const saveAddButton = document.querySelector('#add-save');
+
+const addButton = document.querySelector('#button-add');
+
+const profileNameElement = document.querySelector('#profile-name'); //выводим это все на страницу
+
+const profileDescriptionElement = document.querySelector('#profile-description');
+
+const userName = document.querySelector('#user-name'); // первое поле ввода
+
+const userPost = document.querySelector('#user-post'); //второе поля ввода
+
+const popupWindow = document.querySelector('.popup__window');
+
+const templateElement = document.querySelector('.element__template').content;
+
+const templateElements = document.querySelector('.elements');
+
+userName.value = profileNameElement.textContent;
+userPost.value = profileDescriptionElement.textContent;
+
+function openPopup(popup) {
   //функция открытия
   popup.classList.add('popup_open');
-  userName.value = profileNameElement.textContent;
-  userPost.value = profileDescriptionElement.textContent;
 }
 
-function closePopup() {
+function closePopup(popup) {
   //функция закрытия
   popup.classList.remove('popup_open');
 }
 
-openPopupButton.addEventListener('click', openPopup);
-//открытие попапа на кнопку
+function openPopupAdd() {
+  popup.classList.add('popup_add');
+}
 
-closePopupButton.addEventListener('click', closePopup);
-//закрытие попапа на кнопку
+editButton.addEventListener('click', function () {
+  openPopup(profilePopup);
+});
+
+addButton.addEventListener('click', function () {
+  openPopup(addPopup);
+});
+
+closeProfileButton.addEventListener('click', function () {
+  closePopup(profilePopup);
+});
+
+closeAddButton.addEventListener('click', function () {
+  closePopup(addPopup);
+});
 
 function handleFormSubmit(evt) {
   //замена дефолтных данных на новые значения
   evt.preventDefault();
   profileNameElement.textContent = userName.value;
   profileDescriptionElement.textContent = userPost.value;
-  closePopup();
+  closePopup(profilePopup);
 }
 
 popupWindow.addEventListener('submit', handleFormSubmit); //слушатель событий для отправки формы
+
+function createPlace(element) {
+  const templateElementCopy = templateElement.cloneNode(true);
+  const templateImage = templateElementCopy.querySelector('.element__image');
+  const templateTitle = templateElementCopy.querySelector('.element__group-title');
+  templateImage.setAttribute('src', element.link);
+  templateTitle.textContent = element.name;
+  return templateElementCopy;
+}
+
+function addPopupInitial(item) {
+  const element = createPlace(item);
+  templateElements.prepend(element);
+}
+
+initialCards.forEach(addPopupInitial);
+
+function createCard(evt) {
+  evt.preventDefault();
+  const arrayCard = {
+    name: addName.value,
+    link: addLink.value
+  };
+  addPopupInitial(arrayCard);
+  evt.target.reset();
+  closePopup(addPopup);
+}
+
+addButton.addEventListener('submit', createCard);
